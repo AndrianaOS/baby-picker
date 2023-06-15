5807983e7a7066df936e11b04e67d8e8b6665f1e
Andriana <andrianasaffo@gmail.com>
2023-06-15 12:36:39 +0100

    Include fetch function
    Fetch function to include names added. Some previous code commented out until bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import BabyNames from "./BabyNames";
 import Heading from "./Heading";
-import BabyNamesData from "./directory/babyNamesData.json";
-import SearchInput from "./SearchInput";
+// import BabyNamesData from "./directory/babyNamesData.json";
+// import SearchInput from "./SearchInput";
+import AddNames from "./AddNames";
 
 function App() {
-  const [names, setNames] = useState(BabyNamesData);
+  const [names, setNames] = useState([]);
 
-  function Search(searchVal) {
-    const searchValue = searchVal.toLowerCase();
-    const filteredNames = BabyNamesData.filter((eachName) => {
-      return eachName.name.toLowerCase().includes(searchValue);
-    });
-    console.log(filteredNames);
-    setNames(filteredNames, searchVal);
+  // function Search(searchVal) {
+  //   const searchValue = searchVal.toLowerCase();
+  //   const filteredNames = BabyNamesData.filter((eachName) => {
+  //     return eachName.name.toLowerCase().includes(searchValue);
+  //   });
+  //   console.log(filteredNames);
+  //   setNames(filteredNames, searchVal);
+  // }
+
+  function getAllNames() {
+    fetch("http://localhost:4000/names")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setNames(data);
+      })
+      .catch((error) => console.log(error));
   }
+
+  useEffect(() => {
+    getAllNames();
+  }, []);
 
   return (
     <div className="App">
       <Heading />
-      <SearchInput search={Search} />
+      <AddNames setNames={setNames} />
+      {/* <SearchInput search={Search} /> */}
       <BabyNames names={names} />
     </div>
   );

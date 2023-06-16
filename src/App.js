4dbce72149db30b24633953401b809b6fcb3e4dc
Andriana <andrianasaffo@gmail.com>
2023-06-16 14:56:20 +0100

    Edit code for search function
    Edit code to get all names if search function is not active

--- src/App.js
+++ src/App.js
@@ -4,27 +4,29 @@ import "./App.css";
 import BabyNames from "./BabyNames";
 import Heading from "./Heading";
 // import BabyNamesData from "./directory/babyNamesData.json";
-// import SearchInput from "./SearchInput";
+import SearchInput from "./SearchInput";
 import AddNames from "./AddNames";
 
 function App() {
-  const [names, setNames] = useState([]);
+  const [allNames, setAllNames] = useState([]);
+  const [searchResults, setSearchResults] = useState(allNames);
 
-  // function Search(searchVal) {
-  //   const searchValue = searchVal.toLowerCase();
-  //   const filteredNames = BabyNamesData.filter((eachName) => {
-  //     return eachName.name.toLowerCase().includes(searchValue);
-  //   });
-  //   console.log(filteredNames);
-  //   setNames(filteredNames, searchVal);
-  // }
+  function Search(searchVal) {
+    const searchValue = searchVal.toLowerCase();
+    const filteredNames = allNames.filter((eachName) => {
+      return eachName.name.toLowerCase().includes(searchValue);
+    });
+    console.log(filteredNames);
+    setSearchResults(filteredNames, searchVal);
+  }
 
   function getAllNames() {
     fetch("http://localhost:4000/names")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setNames(data);
+        setAllNames(data);
+        setSearchResults(data);
       })
       .catch((error) => console.log(error));
   }
@@ -36,9 +38,9 @@ function App() {
   return (
     <div className="App">
       <Heading />
-      <AddNames setNames={setNames} />
-      {/* <SearchInput search={Search} /> */}
-      <BabyNames names={names} />
+      <AddNames setNames={setAllNames} />
+      <SearchInput search={Search} />
+      <BabyNames names={searchResults} />
     </div>
   );
 }

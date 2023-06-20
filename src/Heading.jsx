16e01484e24dd20ca8930234a0cd3fcd1c736022
Andriana <andrianasaffo@gmail.com>
2023-06-20 16:07:05 +0100

    Add image and animation to header

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -1,7 +1,20 @@
+import "animate.css";
+
 function Heading() {
   return (
     <header className="App-header">
-      <h1>Baby Name Picker</h1>
+      <img
+        src="stork-baby.png"
+        alt="stock carrying a baby"
+        id="stork-baby"
+        className="animate__animated
+        animate__swing
+        animate__slow
+        animate__infinite"
+      />
+      <h1>WELCOME TO ANDRIANA'S BABY NAME NEST</h1>
+      <h2>Discover the Perfect Name for Your Bundle of Joy</h2>
+      <h3>Scroll down to see names and submit a name below</h3>
     </header>
   );
 }
